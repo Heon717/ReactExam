0758f7236f5fdf3700af941389fe4fad04aae034
Heon717 <fiia2001@naver.com>
2021-06-07 03:53:33 +0900

    상품 컴포넌트 수정해야함

--- ReactExam/shop/src/App.js
+++ ReactExam/shop/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
+import {Navbar,Container, Nav, Button }from 'react-bootstrap';
 import './App.css';
+import Content from './Content.js';
+import data from './data.js';
 
 function App() {
+
+  let [shoes,setShoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
+
+    <div className="dsc">
+      <h1>20% Season Off</h1>
+      <p> 할인합니다</p>
+      <p>
+        <Button variant="primary">Learn more</Button>
+      </p>
+    </div>
+    
+    <div className="container">
+      <Content shoes={shoes}/>
+    </div>
+
     </div>
   );
 }
 
 export default App;
+ 
