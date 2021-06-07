c8877b420118fac8fc3d0dd117852c31ca83cf86
헌 <81272630+Heon717@users.noreply.github.com>
2021-06-07 17:00:42 +0900

    Add files via upload

---
+++ ReactExam/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoHeader from './TodoHeader';
+import TodoTemplate from './TodoTemplate';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoTemplate/>
+    </div>
+  );
+}
+
+export default App;
